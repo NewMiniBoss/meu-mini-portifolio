5ce737a6a57d1420697bbe88aa9f0ff7cd16b39d
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-21 18:22:26 -0300

    animation menu

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector('[data-menu]');
 const menuItens = document.querySelector('[data-menu="items"]');
+const iconeMenu = document.querySelector('[data-menu="button"]');
 
 menu.addEventListener('click', () => {
-    menuItens.classList.toggle('menu--active');
+    menuItens.classList.toggle('menu__active');
+    iconeMenu.classList.toggle('menu__active-icon');
 })
+
